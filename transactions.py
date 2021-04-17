76598e12610219a39dfc1a59d75a2564ad05b25e
Passi <62151638+PasiXD189@users.noreply.github.com>
2021-04-17 23:50:45 +0200

    Delete transactions.py

--- transactions.py
+++
@@ -1,7 +0,0 @@
-def transactions():
-    t1 = "Michael sends 2 NC to Eli"
-    t2 = "Luke sends 4 NC to Cain"
-    t3 = "Martin sends 67 NC to Passi"
-    t4 = "Thomas sends 0.3 NC to Eli"
-    t5 = "Passi sends 42.8 NC to Martin"
-    t6 = "Cain sends 17 NC to Luke"
